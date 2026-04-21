772baa4669ba8113579bca2e60f73fbb7a66d087
Zeq21 <87974492+Zeq21@users.noreply.github.com>
2026-04-21 01:39:09 +0000

    modificar frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home (){
 
-return <h1>Dessa vez vai dar certo,meu primeiro site no ar vou fazer mais um teste </h1>
+return <h1>Dessa vez vai dar certo,meu primeiro site no ar vou fazer mais um teste
+      meu site está no ar agora
+     
+      </h1>
      s
 
 
